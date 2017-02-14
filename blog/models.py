0498f1e7690e931b9f97dd2fd1d0ac55332d06c1
郭泰竹 <102306037@nccu.edu.tw>
2017-02-15 00:30:05 +0800

    build the blog structure

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model): #定義object
-    author = models.ForeignKey(User) #作者
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     title = models.CharField(max_length=200) #標題
     text = models.TextField() #文章內容
+    photo = models.URLField(blank=True)
     created_date = models.DateTimeField( #發文日期
             default=timezone.now)
-    published_date = models.DateTimeField(  #發文日期
-            blank=True, null=True)
 
     def publish(self): #publish 方法
         self.published_date = timezone.now()
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog.views import hello_world,home, post_detail
 
 urlpatterns = [
+	url(r'^hello/', hello_world ),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
+    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
 ]
